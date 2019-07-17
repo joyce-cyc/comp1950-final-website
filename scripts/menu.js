a65327f43632312ccb5878a4eef36ce8cc1a521d
Joyce Cheung <joyce.cycle@gmail.com>
2019-07-16 20:26:05 -0700

    Edit all the things

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,40 +1,12 @@
 // Toggle Menu
 
-// JavaScript Document
+(function(d) {
+	// d = document
 
-var $btn = $('button');
-var $nav = $('nav');
-var $navUl = $('nav > ul');
+	const $body = d.querySelector('body');
+	const $btn = d.querySelector('.mobile-nav');
 
-$btn.click(function(){
-	$navUl.toggleClass('open');	
-});
-
-// The following JS modified from 
-// JS found at this per: http://codepen.io/laviperchik/pen/dlcBt
-$.fn.accessibleDropDown = function () {
-    
-	var el = $(this);
-
-    /* Make dropdown menus keyboard accessible */
-
-    el.find('a').focus(function() {
-        $(this).parents("li").addClass("hover");
-    }).blur(function() {
-        $(this).parents("li").removeClass("hover");
-    });
-
-}
-
-//$nav.accessibleDropDown();
-
-// (function(d) {
-// 	// d = document
-
-// 	const $body = d.querySelector('body');
-// 	const $btn = d.querySelector('.mobile-nav');
-
-// 	$btn.addEventListener('click', function() {
-// 		$body.classList.toggle('show');
-// 	});
-// })(document);
+	$btn.addEventListener('click', function() {
+		$body.classList.toggle('show');
+	});
+})(document);
