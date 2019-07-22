96b663fb0b8856335615da801837fbb40aeaeb8d
Joyce Cheung <joyce.cycle@gmail.com>
2019-07-22 03:55:08 -0700

    Update javascript to automatic slideshow

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -25,22 +25,9 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+  
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
   slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 8000); // Change image every 8 seconds
 }
-
-// // Automatic Slideshow
-// var slideIndex = 0;
-// showSlides();
-
-// function showSlides() {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none"; 
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1} 
-//   slides[slideIndex-1].style.display = "block"; 
-//   setTimeout(showSlides, 8000); // Change image every 8 seconds
-// }
